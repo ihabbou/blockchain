45d9e569ba697bce4bd9c3da8a3f1c911bdc1b41
ihabbou <ihabbou@users.noreply.github.com>
2019-06-14 19:40:43 +0200

    Election contract deployed to the network

---
+++ contracts/Election.sol
@@ -0,0 +1,30 @@
+pragma solidity ^ 0.5 .0;
+
+contract Election {
+
+	struct Candidate {
+		uint id;
+		string name;
+		uint voteCount;
+	}
+
+	// use mapping to get candidates details
+	mapping (uint => Candidate) public candidates;
+
+	// use public variable to have the number of candidates
+	uint public candidateCount;
+
+	//constructor
+	constructor() public {
+		addCandidate("Erika Mustermann");
+		addCandidate("Max Mustermann");
+	}
+
+	// function to add candidates to the election
+	function addCandidate(string memory _name) private {
+		candidateCount++;
+		candidates[candidateCount] = Candidate(candidateCount, _name, 0);
+	}
+
+
+}
