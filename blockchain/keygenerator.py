f6e84ec507c38dd1de7c5177119e3d2bd9124479
ihabbou <ihabbou@users.noreply.github.com>
2019-07-27 15:49:03 +0200

    Deleted old files

--- blockchain/keygenerator.py
+++
@@ -1,8 +0,0 @@
-from ecdsa import SigningKey, VerifyingKey, SECP256k1
-
-sk = SigningKey.generate(curve=SECP256k1)  # uses NIST192p
-vk = sk.get_verifying_key()
-signature = sk.sign("message")
-assert vk.verify(signature, "message")
-
-VerifyingKey
