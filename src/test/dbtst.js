f9283dac4ed80920be18846c1ce2dff7846bed70
ihabbou <ihabbou@users.noreply.github.com>
2019-08-17 18:08:36 +0200

    Display the blockchain

--- src/test/dbtst.js
+++ src/test/dbtst.js
@@ -1,13 +1,13 @@
 var level = require('level'); 
 
-var db = level("./tmp/data_1");
+var db = level("./tmp/data_2");
 
-console.log('Displaying the entire blockchain from node 1: ');
-console.log('This contains failed blocks');
+console.log('Displaying the entire blockchain from node 2: ');
+console.log('This may contain failed blocks');
 
 db.createReadStream({ })
 	.on('data', function(data) {
-		if (JSON.parse(JSON.parse(JSON.stringify(data)).value).version=== 0) {
+		if (JSON.parse(JSON.parse(JSON.stringify(data)).value).version === 0) {
 			console.log(data);
 			console.log();
 		}
